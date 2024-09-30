45b17517b2c74ae7fc758ce204810d1ddd298679
HDegroote <75906619+HDegroote@users.noreply.github.com>
2024-09-30 20:53:58 +0200

    Dummy tests

---
+++ test.js
@@ -0,0 +1,20 @@
+const t = require('brittle')
+
+t('passing test', t => {
+  t.pass('okay')
+})
+
+t('failing test', t => {
+  t.is(1, 1, 'ok')
+  t.is(1, 2, 'oops')
+  t.is(2, 2, 'yes')
+})
+
+t('passing test 2', t => {
+  t.pass('okay')
+})
+
+t('error test', t => {
+  t.pass('okay so far')
+  throw new Error('I break stuff')
+})
